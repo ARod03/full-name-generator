a3ba02d7a5c485eb9ea4563528da6f7fec67a46b
adrianrodriguez <ardgz2003@gmail.com>
2026-04-14 15:17:57 -0500

    Finished work

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -1,4 +1,43 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class FullNameApplication {
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        //Prompt user to provide first name
+        System.out.println("Enter your first name:");
+        String firstName = scanner.nextLine().trim();
+
+        //Prompt user to provide middle name
+        System.out.println("Enter your middle name:");
+        String middleName = scanner.nextLine().trim();
+
+        //Prompt user to provide last name
+        System.out.println("Enter your last name:");
+        String lastName = scanner.nextLine().trim();
+
+        //Prompt user to provide suffix
+        System.out.println("Enter your suffix:");
+        String suffix = scanner.nextLine().trim();
+
+        String fullName = firstName;
+
+        //Include middle name if it was provided
+        if (!middleName.equals("")) {
+            fullName += " " + middleName;
+        }
+        fullName += " " + lastName;
+
+        //Include suffix if it was provided
+        if (!suffix.equals("")) {
+            fullName += ", " + suffix;
+        }
+        //Print out full name
+        System.out.println("Full name: " + fullName);
+    }
+
 }
